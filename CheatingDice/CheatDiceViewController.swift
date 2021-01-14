936af471d237c649202cdaa3f905435c92c0413c
sasama <mametaku3@yahoo.co.jp>
2021-01-15 00:20:17 +0900

    Add：CheatDiceViewControllerを追加
    補足：イカサマサイコロ用のViewController

---
+++ CheatingDice/CheatDiceViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CheatDiceViewController.swift
+//  CheatingDice
+//
+//  Created by 笹間巧馬 on 2021/01/14.
+//
+
+import UIKit
+
+class CheatDiceViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var result: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
