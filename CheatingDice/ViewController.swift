298261c73961399aeb48febd18e56a6673af546f
sasama <mametaku3@yahoo.co.jp>
2021-01-14 21:21:27 +0900

    Add：サイコロ用のランダムで数字を生成しラベルに反映させるrollメソッドを追加 Add：resultラベルを追加

--- CheatingDice/ViewController.swift
+++ CheatingDice/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func roll() {
+        
+        result.text = String(Int.random(in: 1...6))
+    }
+    
+    
+    @IBAction func diceRoll(_ sender: Any) {
+        roll()
+    }
+    
 }
 
