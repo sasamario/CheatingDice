c321b3bdffc81f693a7352992e2254dbbf0bf131
sasama <mametaku3@yahoo.co.jp>
2021-01-18 21:15:59 +0900

    Add：サイコロ結果を表示するためのUIImageViewを追加 Add：動画再生後の処理を追加 Add：UIView（動画用のView）のzPositionを-1に設定する処理を追加（動画再生後に同じ位置に画像を表示させたいため、動画Viewを背景に設定）

--- CheatingDice/ViewController.swift
+++ CheatingDice/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rollButton: UIButton!
     
+    @IBOutlet weak var diceResultImage: UIImageView!
+    
     var player = AVPlayer()
     
     override func viewDidLoad() {
@@ -30,6 +32,9 @@ class ViewController: UIViewController {
     
     //サイコロを振るボタン
     @IBAction func diceRoll(_ sender: Any) {
+        //動画が流れる際は、画像を非表示にしたいため
+        diceResultImage.image = nil
+        
         result.text = "確認中"
         
         //無効化
@@ -40,7 +45,17 @@ class ViewController: UIViewController {
         
         //遅延処理（動画再生後に実行）
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.roll()
+            var diceNumber = 0
+            
+            diceNumber = Int.random(in: 1...6)
+            
+            //resultLabelに反映
+            self.result.text = String(diceNumber)
+            
+            //出た目のサイコロ画像を表示する
+            self.diceResultImage.image = UIImage(named: "dice\(diceNumber)")
+            
+            //サイコロを振るボタンを有効化
             self.rollButton.isEnabled = true
         }
     }
@@ -53,6 +68,10 @@ class ViewController: UIViewController {
         //AVPlayer用のレイヤーを生成
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = self.diceResultView.bounds
+        
+        //画像を表示するImageViewより高さを低くするため
+        playerLayer.zPosition = -1
+        
         self.diceResultView.layer.addSublayer(playerLayer)
         
         //再生
